548df4b7ed4e29ae445cef13f92a6a7f37646ff8
Gyorgy Kovesdi <kgy@teledigit.eu>
2013-06-29 11:49:43 +0200

    Config header is added

---
+++ include/public/glesly/config.h
@@ -0,0 +1,21 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * Project:     Glesly: my GLES-based rendering library
+ * Purpose:     Generic coinfiguration info
+ * Author:      György Kövesdi (kgy@teledigit.eu)
+ * Licence:     GPL (see file 'COPYING' in the project root for more details)
+ * Comments:    
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef __GLESLY_INCLUDE_PUBLIC_GLESLY_CONFIG_H_INCLUDED__
+#define __GLESLY_INCLUDE_PUBLIC_GLESLY_CONFIG_H_INCLUDED__
+
+#ifndef CONFIG_BASE_DIR
+// It is defined externally for release build, this definition is only for development:
+#define CONFIG_BASE_DIR "./config/data/"
+#endif
+
+#endif /* __GLESLY_INCLUDE_PUBLIC_GLESLY_CONFIG_H_INCLUDED__ */
+
+/* * * * * * * * * * * * * End - of - File * * * * * * * * * * * * * * */
