d390d86de324a6c9b6e6ff1593bc3cfb55d4f72f
Gyorgy Kovesdi <gyorgy@yahoo-inc.com>
2014-10-09 09:13:59 +0200

    Configurable 32-bit pixel format

---
+++ src/format.cpp
@@ -0,0 +1,17 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * Project:     
+ * Purpose:     
+ * Author:      
+ * License:     GPL (see file 'COPYING' in the project root for more details)
+ * Comments:    
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include "format.h"
+
+/// The 32-bit pixel format used by the software renderer backend
+/*! If the default value is not suitable, it can be overwritten by the backend before start. */
+GLenum Glesly::PIXEL32FORMAT = GL_RGBA;
+
+/* * * * * * * * * * * * * End - of - File * * * * * * * * * * * * * * */
